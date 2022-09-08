0ab94a094cfccafe56a82829fd3abac0ab5fbc79
David_Nwoye <d.nwoye@yahoo.com>
2022-09-08 08:22:10 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  *  * main - prints to string
@@ -8,6 +9,7 @@
  *    * Return: 1
  *
  *     */
+
 int main(void)
 {
 char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
